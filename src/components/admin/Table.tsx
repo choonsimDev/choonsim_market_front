18fa40a7793a5a8a4b6bdbcd03967dc54c7260c0
남정현 <namjeonghyeon@namjeonghyeon-ui-MacBookAir.local>
2024-07-11 14:26:32 +0900

    Mod Table createdAt

--- src/components/admin/Table.tsx
+++ src/components/admin/Table.tsx
@@ -192,13 +192,8 @@ const Table: React.FC<TableProps> = ({ title, data }) => {
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    const formattedDate = date.toISOString().split(".")[0].replace("T", " ");
+    return formattedDate;
   };
 
   return (
