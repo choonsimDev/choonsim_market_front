e4280fed90c0c4c83f326bffd6690bbb16376098
남정현 <namjeonghyeon@namjeonghyeon-ui-MacBookAir.local>
2024-07-26 15:32:55 +0900

    mod env

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
-import { getCookie } from 'cookies-next';
+import axios from "axios";
+import { getCookie } from "cookies-next";
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
+const BASE_URL = `https://port-0-choonsim-market-back-lyscrsym8c0a1309.sel4.cloudtype.app/`;
 
-const jwt = getCookie('jwt');
+const jwt = getCookie("jwt");
 
 const headers = {
-    'Content-Type': 'application/json',
-    ...(jwt && { 'Authorization': `Bearer ${String(jwt)}` })
+  "Content-Type": "application/json",
+  ...(jwt && { Authorization: `Bearer ${String(jwt)}` }),
 };
 
 const axiosClient = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-    headers,
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers,
 });
 
 export default axiosClient;
