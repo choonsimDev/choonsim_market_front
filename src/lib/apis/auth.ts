851a8d73d76336df13712388fe20ea85a0f4f466
남정현 <namjeonghyeon@namjeonghyeon-ui-MacBookAir.local>
2024-08-05 21:24:46 +0900

    Mod backend url

--- src/lib/apis/auth.ts
+++ src/lib/apis/auth.ts
@@ -4,7 +4,7 @@ import axiosClient from "../axiosClient";
 
 // const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/auth`;
 // const BASE_URL = `http://localhost:3500/auth`;
-const BASE_URL = `https://port-0-choonsim-market-back-lyscrsym8c0a1309.sel4.cloudtype.app//auth`;
+const BASE_URL = `https://port-0-choonsim-market-back-lyscrsym8c0a1309.sel4.cloudtype.app/auth`;
 
 // export const login = async (code: string) => {
 //   return axios.post(`${BASE_URL}/login`, { code }, { withCredentials: true });
